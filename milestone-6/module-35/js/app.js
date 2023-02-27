47560099897f043e116c8d6ddb57e4e8137ce454
Prosen Biswas <webprosen@gmail.com>
2023-02-28 00:53:17 +0600

    gadget & gear

--- milestone-6/module-35/js/app.js
+++ milestone-6/module-35/js/app.js
@@ -1,48 +1,88 @@
-const loadPhones = async(searchText) => {
-    console.log(searchText);
-    if(searchText == null || searchText == ''){
-        searchText = 'iphone'
+// Fetch API
+const loadPhones = async(searchText, dataLimit) => {
+    if(searchText == null){
+        searchText = 'iphone';
     }
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data);
+    displayPhones(data.data, dataLimit);
 }
 
-const displayPhones = phones => {
-
+// Display data on homepage
+const displayPhones = (phones, dataLimit) => {
+    console.log(dataLimit);
     const phonesContainer = document.getElementById('phones-container');
     phonesContainer.innerHTML = '';
-    if(phones.length != 0){
-        phones.forEach(phone => {
-            console.log(phone);
-            const phoneDiv = document.createElement('div');
-            phoneDiv.classList.add('col');
 
-            phoneDiv.innerHTML = `
-                <div class="card p-4">
-                    <img src="${phone.image}" class="" alt="" width="160">
-                    <div class="card-body">
-                        <h5 class="card-title">${phone.phone_name}</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
-                </div>
-            `;
-            phonesContainer.appendChild(phoneDiv);
-        });
+    // Limit data & Show all
+    const loadMore = document.getElementById('load-more');
+    if(dataLimit && phones.length > dataLimit){
+        phones = phones.slice(0, dataLimit);
+        loadMore.classList.remove('d-none');
     } else {
-        phonesContainer.innerHTML = `
-            <div class="alert alert-danger mx-auto" role="alert">
-                No result found!
+        loadMore.classList.add('d-none');
+    }
+
+    // Not found message
+    const notFound = document.getElementById('not-found-message');
+    if(phones.length === 0){
+        notFound.classList.remove('d-none');
+    } else {
+        notFound.classList.add('d-none');
+    }
+
+    // Item added here
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+            <div class="card p-4">
+                <img src="${phone.image}" class="" alt="" width="160">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
             </div>
         `;
+        phonesContainer.appendChild(phoneDiv);
+    });
+    // Loading off
+    loadingSpinner(false);
+}
+
+const processSearch = (dataLimit) => {
+    // Loading start
+    loadingSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText, dataLimit);
+}
+
+// Search button 
+document.getElementById('btn-search').addEventListener('click', function(){
+    processSearch(5);
+});
+
+document.getElementById('search-field').addEventListener('keydown', function(event){
+    if(event.key == 'Enter'){
+        processSearch(5);
+    }
+});
+
+// Loading spinner
+const loadingSpinner = isLoading => {
+    const loader = document.getElementById('loader');
+    if(isLoading){
+        loader.classList.remove('d-none');
+    } else {
+        loader.classList.add('d-none');
     }
 }
 
-document.getElementById('search-field').addEventListener('keyup', function(){
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    loadPhones(searchText);
+// Load more 
+document.getElementById('load-more').addEventListener('click', function(){
+    processSearch();
 });
 
 loadPhones();
